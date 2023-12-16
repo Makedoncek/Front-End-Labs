d5c8ec0c80aec13840b48a41fee8808220fe6e7e
Makedoncek <123456v64@gmail.com>
2023-12-16 02:47:34 +0200

    init dev branch

--- Lab7/my-app/src/App.js
+++ Lab7/my-app/src/App.js
@@ -1,16 +1,24 @@
 // App.js
 import React from 'react';
-import Header from './components/Header';
-import Content from './components/Content';
-import Image from './components/Image';
+import { BrowserRouter as Router, Route, Switch, Link } from 'react-router-dom';
+import Task1 from './components/main-components/Task1';
+import Task2 from './components/main-components/Task2';
 
 function App() {
   return (
-    <div>
-      <Header />
-      <Content />
-      <Image />
-    </div>
+    <Router>
+      <div>
+        <h2>Лабораторна робота 7:</h2>
+        <Link to="/task1">Завдання 1</Link>
+        <br />
+        <Link to="/task2">Завдання 2</Link>
+
+        <Switch>
+          <Route path="/task1" component={Task1} />
+          <Route path="/task2" component={Task2} />
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
