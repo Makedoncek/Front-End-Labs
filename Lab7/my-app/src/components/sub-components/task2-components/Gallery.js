919e38db942083f4c21ed9a6484562d6d7a6e8eb
Makedoncek <123456v64@gmail.com>
2023-12-19 16:12:10 +0200

    task 2 completed

--- Lab7/my-app/src/components/sub-components/task2-components/Gallery.js
+++ Lab7/my-app/src/components/sub-components/task2-components/Gallery.js
@@ -24,27 +24,24 @@ import '../../../components-styles/Gallery.css'
 
 const Gallery = () => {
   const goodsData = [
-    { id: 1, photo: vinland, name: 'Манга "Vinland Saga"', price: 100 },
-    { id: 2, photo: assasination, name: 'Манга "Assasination Classroom"', price: 150 },
-    { id: 3, photo: attack, name: 'Манга "Attack on Titan"', price: 150 },
-    { id: 4, photo: berserk, name: 'Манга "Berserk"', price: 150 },
-    { id: 5, photo: chainsaw, name: 'Манга "Chainsaw Man"', price: 150 },
-    { id: 6, photo: classroom, name: 'Манга "Classroom"', price: 150 },
-    { id: 7, photo: grand, name: 'Манга Grand Blue', price: 150 },
-    { id: 8, photo: astro, name: 'Манга Astro Boy', price: 150 },
-    { id: 9, photo: fairy, name: 'Манга Fairy Tail', price: 150 },
-    { id: 10, photo: gintama, name: 'Манга Gintama', price: 150 },
-    { id: 11, photo: jujutsu, name: 'Манга Jujutsu Kaisen', price: 150 },
-    { id: 12, photo: hunter, name: 'Манга Hunter x Hunter', price: 150 },
-    { id: 13, photo: naruto, name: 'Манга Naruto', price: 150 },
-    { id: 14, photo: pluto, name: 'Манга Pluto', price: 150 },
-    { id: 15, photo: monster, name: 'Манга Monster', price: 150 },
-    { id: 16, photo: solo, name: 'Манга Solo Levelling', price: 150 },
-    { id: 17, photo: owari, name: 'Манга Owari no Seraph', price: 150 },
-    { id: 18, photo: kimetsu, name: 'Манга Kimetsu no Yaiba', price: 150 },
-
-   
-    
+    { id: 1, photo: vinland, name: 'Манга "Vinland Saga"', price: 250 },
+    { id: 2, photo: assasination, name: 'Манга "Assasination Classroom"', price: 275 },
+    { id: 3, photo: attack, name: 'Манга "Attack on Titan"', price: 300 },
+    { id: 4, photo: berserk, name: 'Манга "Berserk"', price: 240 },
+    { id: 5, photo: chainsaw, name: 'Манга "Chainsaw Man"', price: 265 },
+    { id: 6, photo: classroom, name: 'Манга "Classroom of the Elite"', price: 200 },
+    { id: 7, photo: grand, name: 'Манга "Grand Blue"', price: 270 },
+    { id: 8, photo: astro, name: 'Манга "Astro Boy"', price: 200 },
+    { id: 9, photo: fairy, name: 'Манга "Fairy Tail"', price: 250 },
+    { id: 10, photo: gintama, name: 'Манга "Gintama"', price: 185 },
+    { id: 11, photo: jujutsu, name: 'Манга "Jujutsu Kaisen"', price: 205 },
+    { id: 12, photo: hunter, name: 'Манга "Hunter x Hunter"', price: 215 },
+    { id: 13, photo: naruto, name: 'Манга "Naruto"', price: 225 },
+    { id: 14, photo: pluto, name: 'Манга "Pluto"', price: 230 },
+    { id: 15, photo: monster, name: 'Манга "Monster"', price: 250 },
+    { id: 16, photo: solo, name: 'Манга "Solo Leveling"', price: 260 },
+    { id: 17, photo: owari, name: 'Манга "Owari no Seraph"', price: 235 },
+    { id: 18, photo: kimetsu, name: 'Манга "Kimetsu no Yaiba"', price: 290 } 
   ];
 
   return (
